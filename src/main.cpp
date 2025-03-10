2609aca22a19861d79ad0889a2dacc8b59003bcb
Hamed Bakhsh <hamed.bakhsh@student.manchester.ac.uk>
2025-03-10 16:08:48 +0000

    pid testing code

--- src/main.cpp
+++ src/main.cpp
@@ -34,23 +34,23 @@ int main(void){
     
 
         // Encoders setup
-        Encoder Encoder1(PA_11,PA_12,0);
-        Encoder Encoder2(PC_7,PA_9,1);
+        Encoder Encoder1(PC_2,PC_3,0);   //right
+        Encoder Encoder2(PC_7,PA_9,1);   //left
 
         Encoder1.initialise();
         Encoder2.initialise();
 
         // Integrator setup
-        Integrator I1(1000.0f);
-        I1.start(callback(&Encoder1,&Encoder::speed_linear));
-        Integrator I2(1000.0f);
-        I2.start(callback(&Encoder2,&Encoder::speed_linear));
+        // Integrator I1(1000.0f);
+        // I1.start(callback(&Encoder1,&Encoder::speed_linear));
+        // Integrator I2(1000.0f);
+        // I2.start(callback(&Encoder2,&Encoder::speed_linear));
 
-        Integrator I3(1000.0f);
-        auto RobotAngularV_Callback = [&]() -> float {
-            return RobotAngularV(Encoder1, Encoder2);
-        };
-        I3.start(RobotAngularV_Callback);
+        // Integrator I3(1000.0f);
+        // auto RobotAngularV_Callback = [&]() -> float {
+        //     return RobotAngularV(Encoder1, Encoder2);
+        // };
+        // I3.start(RobotAngularV_Callback);
 
         // float issue
         char buffer1[50];
@@ -58,50 +58,29 @@ int main(void){
 
         float speed, distance;
 
-        
+        // PID
+        PID Right(1.0f, 1.0f, 0.0f, 0.0f, 4.0f, 0.0f, 100); //float setpoint,float Kp,float Ki,float Kd,float max_out,float min_out,float freq
+        PID Left(1.0f, 1.0f, 0.0f, 0.0f, 4.0f, 0.0f, 100);
+
+        Left.start(callback(&Encoder2,&Encoder::speed_linear));
+        Right.start(callback(&Encoder1,&Encoder::speed_linear));
+
+        float pwmL, pwmR;
 
 
     while(1){
 
-        Motor1.write(1.0f);
-        Motor2.write(1.0f);
+         pwmL = ((Left.get_output() - 4.2)/(-4.28));
+         pwmR = ((Right.get_output() - 4.2)/(-4.28));
 
+        Motor1.write(pwmR);
+        Motor2.write(pwmL);
 
-        wait_us(5000000);
-
-
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
-        Turn(1.57, Motor1, Motor2, I3, Direction1, Direction2);
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
-        Turn(1.57, Motor1, Motor2, I3, Direction1, Direction2);
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
-        Turn(1.57, Motor1, Motor2, I3, Direction1, Direction2);
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
-        
-        Turn(3.1415, Motor1, Motor2, I3, Direction1, Direction2);
-
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
-        Turn(-1.57, Motor1, Motor2, I3, Direction1, Direction2);
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
-        Turn(-1.57, Motor1, Motor2, I3, Direction1, Direction2);
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
-        Turn(-1.57, Motor1, Motor2, I3, Direction1, Direction2);
-        MoveForward(1.0f, Motor1, Motor2, I1, I2);
+        floatToString(Left.get_output(), buffer1);
+        floatToString(Right.get_output(), buffer2);
 
 
 
-
-        I3.reset();
-        
-        while(1){    
-        
-        floatToString(RobotAngularV(Encoder1, Encoder2), buffer1);
-        floatToString(I3.getIntegral(), buffer2);
-        printf("AngularV: %s,  Angle: %s,  R_direction: %d,  L_direction: %d\n", buffer1, buffer2, Encoder1.direction, Encoder2.direction);
-
-        wait_us(100000);
-
-     
+        printf("Left PID out: %s,   Right PID out %s \n", buffer1, buffer2);
     }
-}
 }
